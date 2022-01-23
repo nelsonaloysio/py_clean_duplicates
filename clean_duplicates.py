3242a638fb4346adfdb6cd4575389d924908f589
Nelson Aloysio <26492660+nelsonaloysio@users.noreply.github.com>
2022-01-23 20:09:06 +0000

    Added `-v`, `--invert` argument and tweaked IndexError handling

--- clean_duplicates.py
+++ clean_duplicates.py
@@ -5,36 +5,40 @@
 Cleans duplicate lines from file by appending line
 or field value to a set() and comparing its length.
 
-usage: clean_duplicates [-h] [-o OUTPUT] [-c COLUMN] [-d DELIMITER]
-                        [-q {0,1,2,3}] [-e ENCODING] [--index-ignore]
-                        [--max-field-size-limit] [--output-ignored]
-                        input
+usage: clean_duplicates [-h] [-o OUTPUT_NAME] [-c COLUMN] [-d DELIMITER]
+                        [-q {0,1,2,3}] [-e ENCODING] [-v]
+                        [--consider-index-errors]
+                        [--dump-index-errors OUTPUT_ERRORS_NAME]
+                        [--max-field-size]
+                        input_name
 
 positional arguments:
-  input                 input file name
+  input_name            input file name
 
 optional arguments:
   -h, --help            show this help message and exit
-  -o OUTPUT, --output OUTPUT
+  -o OUTPUT_NAME, --output-name OUTPUT_NAME
                         output file name
   -c COLUMN, --column COLUMN
                         column index or title to find duplicate values
   -d DELIMITER, --delimiter DELIMITER
                         column field delimiter
   -q {0,1,2,3}, --quoting {0,1,2,3}
-                        text quoting {0: 'minimal', 1: 'all', 2: 'non-numeric', 3: 'none'}
+                        text quoting {0: 'minimal', 1: 'all', 2: 'non-
+                        numeric', 3: 'none'}
   -e ENCODING, --encoding ENCODING
                         file encoding (default: utf-8)
-  --index-ignore        bypass IndexError exceptions
-  --max-field-size-limit
-                        extend field size limit to maximum allowed
-  --output-ignored      lines with IndexError to another file
+  -v, --invert          invert matches to return duplicates only
+  --consider-index-errors
+                        write lines with IndexError to output file
+  --dump-index-errors OUTPUT_ERRORS_NAME
+                        write lines with IndexError to another file
+  --max-field-size      extend field size limit to maximum allowed
 '''
 
 from argparse import ArgumentParser
 from csv import field_size_limit, reader, writer
-from os import remove
-from os.path import basename, isfile, splitext
+from os.path import basename,splitext
 from sys import maxsize, stderr
 
 ENCODING = 'utf-8'
@@ -44,14 +48,24 @@ QUOTING = {0: 'minimal',
            2: 'non-numeric',
            3: 'none'}
 
-def clean_duplicates(input_name, output_name=None,
-    column=None, delimiter=None, quoting=0, encoding=ENCODING,
-    index_ignore=False, max_field_size=False, output_ignored=None):
+def clean_duplicates(
+    input_name,
+    column=None,
+    consider_index_errors=False,
+    delimiter=None,
+    encoding=ENCODING,
+    header=True,
+    invert=False,
+    max_field_size=False,
+    output_errors_name=None,
+    output_name=None,
+    quoting=0,
+) -> None:
     '''
     Perform line duplicate removal.
     '''
+    int_errors = 0
     set_values = set()
-
     quotechar = '"'
 
     if quoting == 3:
@@ -70,67 +84,48 @@ def clean_duplicates(input_name, output_name=None,
     if not output_name:
         name, ext = splitext(basename(input_name))
         output_name = name + '_CLEANED' + ext
-
-    if output_ignored:
-        name, ext = splitext(basename(output_name))
-        errors_name = name + '_IGNORED' + ext
-
-        if isfile(errors_name):
-            remove(errors_name)
+    output_errors_name = output_errors_name or output_name
 
     with open(input_name, 'rt', encoding=encoding) as input_file:
-        file_reader = reader(input_file, delimiter=delimiter, quoting=quoting)
+        file_reader = reader(input_file, delimiter=delimiter, quoting=quoting, quotechar=quotechar)
         header = next(file_reader)
-
-        if isinstance(column, str):
-
-            try: # as index number
-                column = int(column)
-                if column > len(header):
-                    print("Error: invalid column (%s), header has %s columns." % (column, len(header)), file=stderr)
-                    raise SystemExit
-                column = (column - 1)
-
-            except ValueError: # as title
-                if column not in header:
-                    print("Error: invalid column ('%s'), not in header: %s." % (column, header), file=stderr)
-                    raise SystemExit
-                column = header.index(column)
+        column = get_header_index(header, column)
 
         with open(output_name, 'w', newline='', encoding=encoding) as output_file:
             file_writer = writer(output_file, delimiter=delimiter, quoting=quoting, quotechar=quotechar)
             file_writer.writerow(header)
 
-            for line in file_reader:
-                index = file_reader.line_num
-                print('Read %s lines.' % index, end='\r')\
-                if (index/10000).is_integer() else None
+            with open(output_errors_name, 'w', newline='', encoding=encoding) as errors_file:
+                error_writer = writer(errors_file, delimiter=delimiter, quoting=quoting, quotechar=quotechar)
 
-                if line != header:
-                    len_set_values = len(set_values)
+                for line in file_reader:
+                    print(f'Read {file_reader.line_num} lines.', end='\r')\
+                    if (file_reader.line_num/10000).is_integer() else None
 
-                    try: # check if duplicate
-                        set_values.add(line[column] if column != None else tuple(line))
+                    if line != header:
+                        len_set_values = len(set_values)
 
-                        if len(set_values) != len_set_values:
-                            file_writer.writerow(line)
+                        try: # check if duplicate
+                            set_values.add(line[column] if column != None else tuple(line))
 
-                    except IndexError:
-                        if index_ignore:
-                            if output_ignored:
-                                with open(errors_name, 'a', newline='', encoding=encoding) as errors_file:
-                                    errors_writer = writer(errors_file, delimiter=delimiter, quoting=quoting, quotechar=quotechar)
-                                    errors_writer.writerow(line)
-                            continue
-                        raise
+                            if (len(set_values) == len_set_values and invert)\
+                            or (len(set_values) != len_set_values and not invert):
+                                file_writer.writerow(line)
 
-    int_lines_total = file_reader.line_num
-    int_lines_fixed = int_lines_total - len(set_values) - 1
-    int_lines_valid = len(set_values) + 1 # header
+                        except IndexError as e:
+                            print(f'Warning: IndexError on line {file_reader.line_num}.', file=stderr)
+                            int_errors += 1
 
-    print('Read', str(int_lines_total), 'total lines.\n'+\
-          str(int_lines_fixed), 'removed lines.\n'+\
-          str(int_lines_valid), 'lines after cleaning.')
+                            if consider_index_errors:
+                                file_writer.writerow(line)
+
+                            if output_errors_name != output_name:
+                                error_writer.writerow(header) if int_errors == 1 else None
+                                error_writer.writerow(line)
+
+    print(f'Read {file_reader.line_num} total lines.' +
+          f'\n{file_reader.line_num - len(set_values) - (int_errors if consider_index_errors else 0) - 1} invalid lines.' +
+          f'\n{len(set_values) + (int_errors if consider_index_errors else 0) + 1} lines after cleaning.')
 
 def get_file_delimiter(input_name, encoding=ENCODING):
     '''
@@ -145,6 +140,26 @@ def get_file_delimiter(input_name, encoding=ENCODING):
             return i.replace('\\t', '\t')
 
     return '\n'
+
+def get_header_index(header, column=None):
+    '''
+    Returns index number from column in header if string.
+    '''
+    if column:
+        try: # as index number
+            column = int(column)
+            if column > len(header):
+                print(f"Error: invalid column ('{column}'), header has {len(header)} columns.", file=stderr)
+                raise SystemExit
+            column = (column - 1)
+
+        except ValueError: # as title
+            if column not in header:
+                print(f"Error: invalid column ('{column}'), not in header: {header}.", file=stderr)
+                raise SystemExit
+            column = header.index(column)
+
+    return column
 
 def max_field_size_limit(d=10):
     '''
@@ -166,24 +181,17 @@ if __name__ == "__main__":
 
     parser = ArgumentParser()
 
-    parser.add_argument('input', action='store', help='input file name')
-    parser.add_argument('-o', '--output', action='store', help='output file name')
+    parser.add_argument('input_name', action='store', help='input file name')
+    parser.add_argument('-o', '--output-name', action='store', help='output file name')
     parser.add_argument('-c', '--column', action='store', help='column index or title to find duplicate values')
     parser.add_argument('-d', '--delimiter', action='store', help='column field delimiter')
     parser.add_argument('-q', '--quoting', action='store', type=int, choices=QUOTING.keys(), default=0, help='text quoting %s' % QUOTING)
     parser.add_argument('-e', '--encoding', action='store', help='file encoding (default: %s)' % ENCODING)
-    parser.add_argument('--index-ignore', action='store_true', help='bypass IndexError exceptions')
-    parser.add_argument('--max-field-size-limit', action='store_true', help='extend field size limit to maximum allowed')
-    parser.add_argument('--output-ignored', action='store_true', help='lines with IndexError to another file')
+    parser.add_argument('-v', '--invert', action='store_true', help='invert matches to return duplicates only')
+    parser.add_argument('--consider-index-errors', action='store_true', help='write lines with IndexError to output file')
+    parser.add_argument('--dump-index-errors', action='store', dest='output_errors_name', help='write lines with IndexError to another file')
+    parser.add_argument('--max-field-size', action='store_true', help='extend field size limit to maximum allowed')
 
     args = parser.parse_args()
 
-    clean_duplicates(args.input,
-                     args.output,
-                     args.column,
-                     args.delimiter,
-                     args.quoting,
-                     args.encoding,
-                     args.index_ignore,
-                     args.max_field_size_limit,
-                     args.output_ignored)
+    clean_duplicates(**vars(args))
